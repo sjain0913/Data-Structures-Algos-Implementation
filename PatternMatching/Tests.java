1610993c07daa8f925c857a698ec73a175294737
Saumya Jain <samjain0913@gmail.com>
2019-12-16 19:56:49 +0530

    Update and rename PatternMatchingStudentTest.java to Tests.java

--- PatternMatching/Tests.java
+++ PatternMatching/Tests.java
@@ -10,21 +10,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertArrayEquals;
 
-/**
- * This is a basic set of unit tests for ParternMatching.
- *
- * Passing these tests doesn't guarantee any grade on these assignments. These
- * student JUnits that we provide should be thought of as a sanity check to
- * help you get started on the homework and writing JUnits in general.
- *
- * We highly encourage you to write your own set of JUnits for each homework
- * to cover edge cases you can think of for each data structure. Your code must
- * work correctly and efficiently in all cases, which is why it's important
- * to write comprehensive tests to cover as many cases as possible.
- *
- * @author CS 1332 TAs
- * @version 1.0
- */
 public class PatternMatchingStudentTest {
 
     private static final int TIMEOUT = 200;
